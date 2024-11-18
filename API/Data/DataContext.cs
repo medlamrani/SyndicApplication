35f6f013244e3dc07a295253a0b617ce16c19f9b
Mohamed <med.lamrani93@gmail.com>
2024-11-18 16:30:40 +0100

    ef update

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -6,4 +6,22 @@ namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; } 
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    modelBuilder.Entity<UserRole>()
+        .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+    modelBuilder.Entity<UserRole>()
+        .HasOne(ur => ur.User)
+        .WithMany(u => u.UserRoles)
+        .HasForeignKey(ur => ur.UserId);
+
+    modelBuilder.Entity<UserRole>()
+        .HasOne(ur => ur.Role)
+        .WithMany(r => r.UserRoles)
+        .HasForeignKey(ur => ur.RoleId);
+}
 }
