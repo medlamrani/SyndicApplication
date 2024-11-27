56f7f91d36715f3e74e596720f9e3638da0bc63e
Mohamed <med.lamrani93@gmail.com>
2024-11-27 11:09:38 +0100

    more

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-=======
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
->>>>>>> 59dd58d15ab9e8b7e28c9f5f8efca6d75640c159
+
 using API.DTOs;
 using API.Entities;
 using AutoMapper;
@@ -13,16 +7,9 @@ namespace API.Helpers
 {
     public class AutoMapperProfiles : Profile
     {
-<<<<<<< HEAD
         public AutoMapperProfiles()
         {
             CreateMap<AppUser, MemberDto>();
         }
-=======
-        public AutoMapperProfiles() 
-        {
-            CreateMap<AppUser, MemberDto>();
-        } 
->>>>>>> 59dd58d15ab9e8b7e28c9f5f8efca6d75640c159
     }
 }
